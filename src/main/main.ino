d4fe72df036fcbf49d5a25fd6f7befeb4f5f3aa2
jefferson <lisboa.jeff@gmail.com>
2016-05-19 18:14:07 -0300

    Esqueleto do projeto foi adicionado.

---
+++ src/main/main.ino
@@ -0,0 +1,38 @@
+
+/*
+    This file is part of S.S.F.
+
+    This S.S.F is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+#include <MySQL_Encrypt_Sha1.h>
+#include <MySQL_Packet.h>
+#include <MySQL_Connection.h>
+#include <MySQL_Cursor.h>
+
+#include "./classes/Sensores.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
